486a1e044ea8a4e8dfd90f9b0bfb3d161a3ed945
bhakti chavanke <chavankebhakti22@gmail.com>
2026-03-19 03:46:22 +0530

    fix requirements file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import numpy as np
 import tensorflow as tf
 import cv2
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -60,4 +61,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
